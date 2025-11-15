380b9bebdd5f1fabd605336dbc0e13fed2b5e958
Nicolas <>
2025-11-15 20:24:26 +0100

    Remove some getter and setter

--- src/SplineOP.h
+++ src/SplineOP.h
@@ -1,15 +1,14 @@
 #ifndef SPLINEOP_H
 #define SPLINEOP_H
 
-//#include <Rcpp.h> // For binding with R
+#include <Rcpp.h> // For binding with R
 #include <iostream> // WHY?
 #include <algorithm> // USE IT BUT CHEKC WHY
 #include <vector>   // Store changepoints, and other (?)
 #include <limits>   // Use it to have the infinity for the CURRENT_MIN cost
 #include <cmath>   // use it in Faulhaber inside the cost 
 #include <random>      // for random number generation
-//#include <RcppEigen.h> // for storing values in DP loo
-#include <Eigen/Dense>
+#include <RcppEigen.h> // for storing values in DP loo
 #include <optional> // To avoid initializing matrices at construction
 
 #include "QuadraticCost.h" // for computing the cost 
@@ -28,8 +27,6 @@ class SplineOP
         
         std::vector<Eigen::MatrixXd> speeds; // best speed holder
         Eigen::MatrixXd costs;  // matrix of costs
-        Eigen::MatrixXd pruning_costs;  // matrix of costs
-        std::vector<std::vector<size_t>> times_for_states;
         Eigen::MatrixXd initSpeeds; //  to check if matrix or vector ; set of initial speeds;
         std::vector<Eigen::MatrixXd> states; // sets of state for each time 
         
@@ -50,14 +47,10 @@ class SplineOP
                                             size_t rows, 
                                             size_t cols);
         void backtrack_changes();
-        void prunev1(size_t t, double margin);
-        void prunev2(size_t t);
     //public methods and attributes
     public:
         //void set_speeds(const Eigen::MatrixXdXd& speeds);
         void predict(double beta); // predicts with a given penalty
-        void pruningv1(double beta, double margin); // predicts with a given penalty
-        void pruningv2(double beta); // predicts with a given penalty
 
 
         //// Getters
@@ -69,13 +62,7 @@ class SplineOP
         Eigen::MatrixXi get_argmin_i() const {return argmin_i;}
         Eigen::MatrixXi get_argmin_s() const {return argmin_s;}
         double get_segment_cost(int s, int t, Eigen::VectorXd p_s, Eigen::VectorXd p_t, Eigen::VectorXd v_s); // compute a cost with given parameters
-        Eigen::MatrixXd get_pruning_costs() const {return pruning_costs;}
-        std::vector<std::vector<size_t>> get_non_pruned_times() const {return times_for_states;}
-        //setters 
-        void set_states(std::vector<Eigen::MatrixXd> new_states);// {states = new_states;}
-        void set_initSpeeds(Eigen::MatrixXd new_initSpeeds);// {initSpeeds = new_initSpeeds;}
-
-
+        
         // Cost getters
         Eigen::MatrixXd get_cumsum_y()   const {return qc.get_cumsum_y();}
         Eigen::MatrixXd get_cumsum_y2()  const {return qc.get_cumsum_y2();}
