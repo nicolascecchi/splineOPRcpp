c2884cec09c693fafe4d534760de9fa39da6f094
Nicolas <>
2025-12-01 14:22:21 +0100

    Add implicit float casting in degree 4

--- src/Faulhaber.cpp
+++ src/Faulhaber.cpp
@@ -11,7 +11,7 @@ double Faulhaber(int n, int deg){
         case 3:
             return 1./4. * n*n*(n+1)*(n+1);
         case 4:
-            return n/5. * (n+1) * (n+1./2.) * (n - (-3+sqrt(21))/6) * (n - (-3-sqrt(21))/6);     
+            return 1./5. * n * (n+1) * (n+1./2.) * (n - (-3+sqrt(21.))/6.) * (n - (-3-sqrt(21.))/6.);     
         default:
             throw std::out_of_range("Degree must be between 1 and 4");
     }
