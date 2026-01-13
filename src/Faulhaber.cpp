d00e72fcf2f044f09159d53631256e9f862e6194
Nicolas <>
2026-01-13 19:15:51 +0100

    Prettify Faulhaber

--- src/Faulhaber.cpp
+++ src/Faulhaber.cpp
@@ -2,16 +2,18 @@
 #include <cmath>
 #include <stdexcept>
 
-double Faulhaber(int n, int deg){
-    switch (deg){
+double Faulhaber(int n, int deg)
+{
+    switch (deg)
+    {
         case 1:
             return 0.5 * n * (n + 1);
         case 2:
-            return 1./6. * n * (n+1) * (2*n + 1);
+            return 1./6. * n * (n + 1) * (2*n + 1);
         case 3:
-            return 1./4. * n*n*(n+1)*(n+1);
+            return 1./4. * n*n*(n + 1)*(n + 1);
         case 4:
-            return 1./5. * n * (n+1) * (n+1./2.) * (n - (-3+sqrt(21.))/6.) * (n - (-3-sqrt(21.))/6.);     
+            return 1./5. * n * (n + 1) * (n + 0.5) * (n - (-3+sqrt(21.))/6.) * (n - (-3-sqrt(21.))/6.);     
         default:
             throw std::out_of_range("Degree must be between 1 and 4");
     }
